ff8f1b0c5d66258d3d4079c6830b915989cfb990
Zachary Champion <zchampion270@gmail.com>
2017-11-17 11:58:40 -0700

    Added the dna file.

---
+++ dna.py
@@ -0,0 +1,53 @@
+# Variable assignments
+a = 0
+b = 0
+c = 0
+d = 0
+a = b
+a = c
+a = d
+b = a
+b = c
+b = d
+c = a
+c = b
+c = d
+d = a
+d = b
+d = c
+
+# Print statements
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Basic Incrementers
+a += 1
+b += 1
+c += 1
+d += 1
+
+# Variable Incrementers
+a += a
+a += b
+a += c
+a += d
+b += a
+b += b
+b += c
+b += d
+c += a
+c += b
+c += c
+c += d
+d += a
+d += b
+d += c
+d += d
+
+# Math operations
+a = -a
+b = -b
+c = -c
+d = -d
